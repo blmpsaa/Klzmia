875c1d50f00d1fd21400f8c8a8a457164ee9f6e6
blmpsaa <bl2rryback@gmail.com>
2025-07-02 05:44:18 -0500

    Update wattyhubintro.lua

--- wattyhubintro.lua
+++ wattyhubintro.lua
@@ -1,5 +1,5 @@
 local BURL = "https://raw.githubusercontent.com/blmpsaa/Klzmia/main/"
-local fn = "wattyhub.ogg"
+local fn = "wattyhub.mp3"
 
 if not isfile(fn) then
     pcall(function() writefile(fn, game:HttpGet(BURL .. fn)) end)
@@ -10,6 +10,7 @@ local success, sid = pcall(function() return getcustomasset(fn) end)
 if success and sid then
     local s = Instance.new("Sound", workspace)
     s.SoundId = sid
+    s.Volume = 6.5
     s:Play()
     s.Ended:Wait()
     s:Destroy()
